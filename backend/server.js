dd231f3921b6a940c583c8246048f078a816cedc
Grace <gracejiayizhang@gmail.com>
2025-01-02 10:54:07 -0500

    placeholder endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
 app.use(cors());
 
-// Test route
-app.get("/", (req, res) => {
-  res.send("Hello, Backend is running!");
+// Load the built in middleware function express.json() to parse JSON data
+// from the req and attach it to req.body
+app.use(express.json());
+app.get("/meals", (req, res) => {
+  res.json({ message: "placeholder for GET /meals" });
+});
+
+app.post("/meals", (req, res) => {
+  res.json({ message: "placeholder for POST /meals", data: req.body });
 });
 
 const PORT = 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
